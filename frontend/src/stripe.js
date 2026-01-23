bacd187296d6a626e7e9e4201c293f31184647b2
Abdullah <yousaf.8666305@gmail.com>
2026-01-24 02:26:50 +0500

    Fix: move Stripe secret to env

--- frontend/src/stripe.js
+++ frontend/src/stripe.js
@@ -1,13 +1,8 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-// export const stripePromise = loadStripe(
-//   import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY || "pk_test_51Sk03zCuXHpZl3JMlrg0WtjRCU8DrZjnneUoy5csxcyo0vElxoiyaJrDwh2nJNUD1wnPgyyzT0JLCeS6n8v8SDx000W8Q1HgNk"
-// );
- 
-
 // Make sure you're using your publishable key
 const stripePromise = loadStripe(
-  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY || "pk_test_51Sk03zCuXHpZl3JMlrg0WtjRCU8DrZjnneUoy5csxcyo0vElxoiyaJrDwh2nJNUD1wnPgyyzT0JLCeS6n8v8SDx000W8Q1HgNk"
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
 );
 
 export { stripePromise };
